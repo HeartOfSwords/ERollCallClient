aaf2222cfe3fc30d09aaf6782359ed94f35e02bd
Lipengpeng <lpp605181746@163.com>
2016-04-25 09:51:45 +0800

    Commit by lipengpeng 16.04.25

--- E-point/app/src/main/java/com/epoint/Activity/Login_activity.java
+++ E-point/app/src/main/java/com/epoint/Activity/Login_activity.java
@@ -24,43 +24,51 @@ import com.snowalker.erollcall.R;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+
 /**
  * @author lipengpeng
- *
  */
 public class Login_activity extends Activity {
 
-    private EditText user_name;
-    private EditText user_pwd;
+    //获取登录界面相关参数
+    private EditText user_name; //获取用户名
+    private EditText user_pwd;  //获取密码
     private RadioGroup radioGroup;
-    private RadioButton user_type;
-    private Button login_button;
+    private RadioButton user_type;  //获取用户身份
+    private Button login_button;    //获取登录按钮
 
-
-    Handler handler = new Handler(){
+    //获取从子线程中传递过来的参数，执行不同的操作
+    //跳转学生菜单页
+    //跳转教师菜单页
+    //身份验证未通过，弹出登录失败信息
+    Handler handler = new Handler() {
         public void handleMessage(Message msg) {
             String result = (String) msg.obj;
 
-            if (msg.arg1==ActivityJumpFlag.UserInfo.STUDENT){
+            if (msg.arg1 == ActivityJumpFlag.UserInfo.STUDENT) {
                 Toast.makeText(Login_activity.this, result, Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(Login_activity.this,StudntActivity.class);
+                Intent intent = new Intent(Login_activity.this, StudntActivity.class);
                 Login_activity.this.startActivity(intent);
-            }else if (msg.arg1==ActivityJumpFlag.UserInfo.TEACHER){
+            } else if (msg.arg1 == ActivityJumpFlag.UserInfo.TEACHER) {
                 Toast.makeText(Login_activity.this, result, Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(Login_activity.this,TeacherResetCounter.class);
+                Intent intent = new Intent(Login_activity.this, TeacherResetCounter.class);
                 Login_activity.this.startActivity(intent);
-            }else {
+            } else {
                 Toast.makeText(Login_activity.this, "登陆失败！", Toast.LENGTH_SHORT).show();
             }
 
-        };
+        }
+
+        ;
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_activity);
 
+        //将login_activity.java加入活动列表List
         ActiveContainer.addActivity(Login_activity.this);
 
         user_name = (EditText) findViewById(R.id.user_name);
@@ -69,20 +77,24 @@ public class Login_activity extends Activity {
 
         login_button = (Button) findViewById(R.id.login_button);
 
+        //为登录按钮设置监听器
         login_button.setOnClickListener(new OnClickListener() {
 
+            //设置获取登录验证返回结果的标志变量
             private String result;
 
             @Override
             public void onClick(View arg0) {
                 user_type = (RadioButton) findViewById(radioGroup
                         .getCheckedRadioButtonId());
-                new Thread(){
+                new Thread() {
                     public void run() {
                         String username = null;
                         Message message = new Message();
                         try {
-                            username = URLEncoder.encode(user_name.getText().toString(),"utf-8");
+
+                            //解决用户名为汉字时的乱码问题，使用URLEncode.encode方法
+                            username = URLEncoder.encode(user_name.getText().toString(), "utf-8");
                         } catch (UnsupportedEncodingException e) {
                             e.printStackTrace();
                         }
@@ -90,28 +102,31 @@ public class Login_activity extends Activity {
                         String userpwd = user_pwd.getText().toString();
                         String usertype = user_type.getText().toString();
 
-                        if (username.trim().equals("") || userpwd.trim().equals("")){
+                        //当用户名和密码为空时执行相关操作，似乎已经没用了，暂留
+                        if (username.trim().equals("") || userpwd.trim().equals("")) {
 
-                        }else if(usertype.equals("学生")){
-                            result = LoginTools.GetLoginResult(ActivityJumpFlag.URL.LOGIN_VERIFY+"student"+"/"+username+"/"+userpwd, NetTools.GET);
-                            if ("登陆成功".equals(result)) {
-                                message.arg1=ActivityJumpFlag.UserInfo.STUDENT;
-                                ActivityJumpFlag.UserInfo.USER_ID=user_name.getText().toString();
-                            }else{
+                        } else if (usertype.equals("学生")) {  //如果登录用户学生为身份，向学生验证接口发送请求
+                            result = LoginTools.GetLoginResult(ActivityJumpFlag.URL.LOGIN_VERIFY + "student" + "/" + username + "/" + userpwd, NetTools.GET);
+                            if ("登陆成功".equals(result)) {  //身份验证通过
+                                message.arg1 = ActivityJumpFlag.UserInfo.STUDENT;   //将学生验证通过标志变量赋值给arg1，方便在handler中进行比较
+                                ActivityJumpFlag.UserInfo.USER_ID = user_name.getText().toString(); //将当前用户的用户名赋值给一个公共静态变量储存
+                            } else {
 
                             }
-                        }else{
+                        } else {
                             result = LoginTools.GetLoginResult(ActivityJumpFlag.URL.LOGIN_VERIFY + "teacher" + "/" + username + "/" + userpwd, NetTools.GET);
-                            if ("登陆成功".equals(result)) {
-                                message.arg1=ActivityJumpFlag.UserInfo.TEACHER;
-                                ActivityJumpFlag.UserInfo.USER_ID=user_name.getText().toString();
-                            }else{
+                            if ("登陆成功".equals(result)) {    //教师登录成功
+                                message.arg1 = ActivityJumpFlag.UserInfo.TEACHER;
+                                ActivityJumpFlag.UserInfo.USER_ID = user_name.getText().toString();
+                            } else {
 
                             }
                         }
-                        message.obj = result;
+                        message.obj = result;   //将返回的结果传递给主线程
                         handler.sendMessage(message);
-                    };
+                    }
+
+                    ;
                 }.start();
 
             }
